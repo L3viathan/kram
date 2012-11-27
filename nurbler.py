00585e5813e9373890d9264075b2bb4c03c8e6e4
L3viathan <github@l3vi.de>
2012-11-27 10:50:42 +0100

    Nurblenurblenurble

---
+++ nurbler.py
@@ -0,0 +1,25 @@
+ # -*- coding: utf8 -*-
+import nltk
+
+
+
+def nurbleize(sentence):
+     allowed=["NNP","NNS","NN"]
+     punct=[".",","]
+     result = []
+     tk=nltk.word_tokenize(sentence)
+     tg=nltk.pos_tag(tk)
+     for (w,p) in tg:
+             if p in allowed:
+                     result +=[" "+w.upper()]
+             elif p in punct:
+                     result +=[w]
+             else:
+                     result +=[" nurble"]
+     return "".join(result)[1:]
+
+example = "Bedrich Smetana was a Czech composer who pioneered the development of a musical style which became closely identified with his country's aspirations to independent statehood. He is thus widely regarded in his homeland as the father of Czech music. Internationally he is best known for his opera The Bartered Bride, for the symphonic cycle Ma vlast ('My Fatherland'), which portrays the history, legends and landscape of the composer's native land, and for his First String Quartet From My Life."
+example2 = "Dieser Satz ist wahr relativ zu einer Belegung A und einer Modellstruktur M, es muss also für alle D aus U gelten."
+
+print(nurbleize(example2))
+#print(nurbleize(example2))
