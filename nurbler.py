fc535c4300b556d68f4fbd35b673ec5fd790ce44
L3viathan <github@l3vi.de>
2012-11-27 10:53:53 +0100

    nurble nurble examples nurble..

--- nurbler.py
+++ nurbler.py
@@ -19,7 +19,5 @@ def nurbleize(sentence):
      return "".join(result)[1:]
 
 example = "Bedrich Smetana was a Czech composer who pioneered the development of a musical style which became closely identified with his country's aspirations to independent statehood. He is thus widely regarded in his homeland as the father of Czech music. Internationally he is best known for his opera The Bartered Bride, for the symphonic cycle Ma vlast ('My Fatherland'), which portrays the history, legends and landscape of the composer's native land, and for his First String Quartet From My Life."
-example2 = "Dieser Satz ist wahr relativ zu einer Belegung A und einer Modellstruktur M, es muss also für alle D aus U gelten."
 
-print(nurbleize(example2))
-#print(nurbleize(example2))
+print(nurbleize(example))
